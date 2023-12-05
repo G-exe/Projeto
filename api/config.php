634a2cf0b74eab31c9f2de7d77100dde128ea465
G-exe <101519702+G-exe@users.noreply.github.com>
2023-12-05 10:32:43 -0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,9 +1,11 @@
 <?php
-$POSTGRES_URL="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com:5432/task_manager";
-$POSTGRES_USER="default";
-$POSTGRES_HOST="ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com";
-$POSTGRES_PASSWORD="W9wu3PHXpZYy";
-$POSTGRES_DATABASE="task_manager";
+POSTGRES_URL="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com:5432/task_manager"
+POSTGRES_PRISMA_URL="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com:5432/task_manager?pgbouncer=true&connect_timeout=15"
+POSTGRES_URL_NON_POOLING="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518.us-east-1.postgres.vercel-storage.com:5432/task_manager"
+POSTGRES_USER="default"
+POSTGRES_HOST="ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com"
+POSTGRES_PASSWORD="W9wu3PHXpZYy"
+POSTGRES_DATABASE="tasks"
 
 $conn = pg_connect("host=$POSTGRES_HOST dbname=$POSTGRES_DATABASE user=$POSTGRES_USER password=$POSTGRES_PASSWORD");
 
