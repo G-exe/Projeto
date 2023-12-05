e801deb733cd6d351b886680f13741b4b9110901
G-exe <101519702+G-exe@users.noreply.github.com>
2023-12-05 10:56:08 -0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -3,11 +3,14 @@ $POSTGRES_URL = "postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518-poole
 $POSTGRES_USER = "default";
 $POSTGRES_HOST = "ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com";
 $POSTGRES_PASSWORD = "W9wu3PHXpZYy";
-$POSTGRES_DATABASE = "task_manager";  // Alterado para task_manager
+$POSTGRES_DATABASE = "task_manager";
 
-$conn = pg_connect("host=$POSTGRES_HOST port=5432 dbname=$POSTGRES_DATABASE user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=require");
+// Obtenha o ID do endpoint a partir do nome do domínio
+$endpointId = 'ep-gentle-flower-83621518-pooler';
+
+$conn = pg_connect("host=$POSTGRES_HOST port=5432 dbname=$POSTGRES_DATABASE user=$POSTGRES_USER password=$POSTGRES_PASSWORD options=--options=--endpoint%3D$endpointId sslmode=require");
 
 if (!$conn) {
-    die("Connection failed: ");
+    die("Connection failed: " . pg_last_error());
 }
 ?>
