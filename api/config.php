814df714fcd718899bea17ab58f4b0e342fbac71
G-exe <101519702+G-exe@users.noreply.github.com>
2023-12-05 10:44:33 -0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,16 +1,13 @@
 <?php
-$POSTGRES_URL="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com:5432/task_manager";
-$POSTGRES_PRISMA_URL="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com:5432/task_manager?pgbouncer=true&connect_timeout=15";
-$POSTGRES_URL_NON_POOLING="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518.us-east-1.postgres.vercel-storage.com:5432/task_manager";
-$POSTGRES_USER="default";
-$POSTGRES_HOST="ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com";
-$POSTGRES_PASSWORD="W9wu3PHXpZYy";
-$POSTGRES_DATABASE="tasks";
+$POSTGRES_URL = "postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com:5432/task_manager";
+$POSTGRES_USER = "default";
+$POSTGRES_HOST = "ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com";
+$POSTGRES_PASSWORD = "W9wu3PHXpZYy";
+$POSTGRES_DATABASE = "task_manager";  // Alterado para task_manager
 
-$conn = new mysqli($POSTGRES_HOST, $POSTGRES_USER, $POSTGRES_PASSWORD, $POSTGRES_DATABASE);
+$conn = pg_connect("host=$POSTGRES_HOST port=5432 dbname=$POSTGRES_DATABASE user=$POSTGRES_USER password=$POSTGRES_PASSWORD");
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
 }
-
 ?>
