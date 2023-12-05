6e1debf68829d6909d83c27021754370f89fd8eb
G-exe <101519702+G-exe@users.noreply.github.com>
2023-12-05 09:33:48 -0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,10 +1,13 @@
 <?php
-$host = 'localhost';
-$user = 'cloud_admin';
-$password = '';
-$database = 'task_manager';
+$POSTGRES_URL="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+$POSTGRES_PRISMA_URL="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15"
+$POSTGRES_URL_NON_POOLING="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-83621518.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+$POSTGRES_USER="default"
+$POSTGRES_HOST="ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com"
+$POSTGRES_PASSWORD="W9wu3PHXpZYy"
+$POSTGRES_DATABASE="task_manager"
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($POSTGRES_HOST, $POSTGRES_USER, $POSTGRES_PASSWORD, $POSTGRES_DATABASE);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
