2927a5aa002c0a2ce218bbb91392fc64e537aa37
G-exe <101519702+G-exe@users.noreply.github.com>
2023-12-05 10:52:28 -0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -5,7 +5,7 @@ $POSTGRES_HOST = "ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-sto
 $POSTGRES_PASSWORD = "W9wu3PHXpZYy";
 $POSTGRES_DATABASE = "task_manager";  // Alterado para task_manager
 
-$conn = pg_connect("host=$POSTGRES_HOST port=5432 dbname=$POSTGRES_DATABASE user=$POSTGRES_USER password=$POSTGRES_PASSWORD");
+$conn = pg_connect("host=$POSTGRES_HOST port=5432 dbname=$POSTGRES_DATABASE user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=require");
 
 if (!$conn) {
     die("Connection failed: ");
