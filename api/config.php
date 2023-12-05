628786fded9371b86e0a58d229561bd72c742c13
G-exe <101519702+G-exe@users.noreply.github.com>
2023-12-05 10:20:06 -0300

    Update config.php

--- api/config.php
+++ api/config.php
@@ -6,7 +6,7 @@ $POSTGRES_URL_NON_POOLING="postgres://default:W9wu3PHXpZYy@ep-gentle-flower-8362
 $POSTGRES_USER="default";
 $POSTGRES_HOST="ep-gentle-flower-83621518-pooler.us-east-1.postgres.vercel-storage.com";
 $POSTGRES_PASSWORD="W9wu3PHXpZYy";
-$POSTGRES_DATABASE="task_manager";
+$POSTGRES_DATABASE="verceldb";
 
 $conn = new mysqli($POSTGRES_HOST, $POSTGRES_USER, $POSTGRES_PASSWORD, $POSTGRES_DATABASE, 30);
 if ($conn->connect_error) {
