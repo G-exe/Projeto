007b0725db20dc00f5ee501328c13dfeb04c76ef
G-exe <101519702+G-exe@users.noreply.github.com>
2023-12-05 10:30:15 -0300

    Update tasks.php

--- api/tasks.php
+++ api/tasks.php
@@ -3,52 +3,30 @@ include 'config.php';
 
 function createTask($title, $description) {
     global $conn;
-    $title = pg_escape_string($title);
-    $description = pg_escape_string($description);
-
     $sql = "INSERT INTO tasks (title, description) VALUES ('$title', '$description')";
-    if (pg_query($conn, $sql) === FALSE) {
-        echo "Erro na inserção: " . pg_last_error($conn);
-    }
+    $conn->query($sql);
 }
 
 function readTasks() {
     global $conn;
-    $tasks = [];
-
     $sql = "SELECT * FROM tasks";
-    $result = pg_query($conn, $sql);
-
-    if ($result === FALSE) {
-        echo "Erro na consulta: " . pg_last_error($conn);
-    } else {
-        while ($row = pg_fetch_assoc($result)) {
-            $tasks[] = $row;
-        }
+    $result = $conn->query($sql);
+    $tasks = [];
+    while ($row = $result->fetch_assoc()) {
+        $tasks[] = $row;
     }
-
     return $tasks;
 }
 
 function updateTask($id, $title, $description) {
     global $conn;
-    $title = pg_escape_string($title);
-    $description = pg_escape_string($description);
-
     $sql = "UPDATE tasks SET title='$title', description='$description' WHERE id=$id";
-    if (pg_query($conn, $sql) === FALSE) {
-        echo "Erro na atualização: " . pg_last_error($conn);
-    }
+    $conn->query($sql);
 }
 
 function deleteTask($id) {
     global $conn;
     $sql = "DELETE FROM tasks WHERE id=$id";
-    if (pg_query($conn, $sql) === FALSE) {
-        echo "Erro na exclusão: " . pg_last_error($conn);
-    }
+    $conn->query($sql);
 }
-
-// Fechar a conexão ao final do script (opcional, dependendo do seu caso de uso)
-pg_close($conn);
 ?>
